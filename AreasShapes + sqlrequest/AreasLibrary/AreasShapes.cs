ce860b485bc7a8381f83b901a87390b0cc805984
timhaX <timaan2000@gmail.com>
2022-05-16 16:42:56 +0300

    mindbox

---
+++ AreasShapes + sqlrequest/AreasLibrary/AreasShapes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace AreasLibrary
+{
+    public class AreasShapes
+    {
+        //Площадь квадратов, прямоугольников и параллелограммов
+        public static double Area(double width, double height)
+        {
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentException("Sides should be > 0");
+            }
+            return width * height;
+        }
+        //Площадь кругов
+        public static double AreaCircle(double radius)
+        {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Radius should be > 0");
+            }
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        //Площадь треугольников
+        public static double AreaTriangle(double sideOne, double sideTwo, double sideThree)
+        {
+            if (sideOne <= 0 || sideTwo <= 0 || sideThree <= 0)
+            {
+                throw new ArgumentException("Sides should be > 0");
+            }
+            double p = (sideOne + sideTwo + sideThree) / 2;
+            if(p < sideOne || p < sideTwo || p < sideThree)
+            {
+                throw new ArgumentException("p should be >= any side of triangles");
+            }
+            return Math.Pow((p * (p - sideOne) * (p - sideTwo) * (p - sideThree)),0.5);
+        }
+        public static double AreaTriangle(double b, double height)
+        {
+            if (b <= 0 || height <= 0)
+            {
+                throw new ArgumentException("Base and height should be > 0");
+            }
+
+            return 0.5 * b * height;
+        }
+        //Проверка прямоугольного треугольника
+        public static bool isRightTriangle(double sideOne, double sideTwo, double sideThree)
+        {
+            if (sideOne <= 0 || sideTwo <= 0 || sideThree <= 0)
+            {
+                throw new ArgumentException("Sides should be > 0");
+            }
+            double maxSide = Math.Max(Math.Max(sideOne, sideTwo), sideThree);
+            return (maxSide * maxSide) == (maxSide == sideOne ? sideTwo * sideTwo + sideThree * sideThree :
+                                            maxSide == sideTwo ? sideOne * sideOne + sideThree * sideThree :
+                                            sideOne * sideOne + sideTwo * sideTwo);
+        }
+    }
+}
---
+++ AreasShapes + sqlrequest/AreasLibraryTest/UnitTest1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AreasLibrary;
+
+namespace AreasLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestArea()
+        {
+            double[,] sides = new double[3, 2] { { 0.1, 0.2 },
+                                                 { 0.3, 0.4 },
+                                                 { 0.5, 0.6 } };
+            for(int i = 0; i < sides.GetLength(0); i++)
+            {
+                double result = sides[i, 0] * sides[i, 1];
+                double fromLibrary = AreasLibrary.AreasShapes.Area(sides[i, 0], sides[i, 1]);
+                Assert.AreEqual(fromLibrary, result, 0.01, "From library = " + fromLibrary + " should be = " + result);            
+            }
+        }
+        [TestMethod]
+        public void TestCircle()
+        {
+            double[] r ={ 0.1, 0.2 ,  0.3, 0.4 };
+            for (int i = 0; i < r.Length; i++)
+            {
+                double result = Math.PI * r[i] * r[i];
+                double fromLibrary = AreasLibrary.AreasShapes.AreaCircle(r[i]);
+                Assert.AreEqual(fromLibrary, result, 0.01, "From library = " + fromLibrary + " should be = " + result);
+            }
+        }
+        [TestMethod]
+        public void TestTriangleThreeSides()
+        {
+            double[,] sides = { { 0.1, 0.2, 0.3 },
+                                { 0.4, 0.5, 0.6 },
+                                { 0.7, 0.8, 0.9 } };
+            for (int i = 0; i < sides.GetLength(0); i++)
+            {                
+                double p = (sides[i, 0] + sides[i, 1] + sides[i, 2]) / 2;                
+                double result = Math.Sqrt(p * (p - sides[i, 0]) * (p - sides[i, 1]) * (p - sides[i, 2]));               
+                double fromLibrary = AreasLibrary.AreasShapes.AreaTriangle(sides[i,0], sides[i,1], sides[i,2]);                
+                Assert.AreEqual(fromLibrary, result, 0.01, "From library = " + fromLibrary + " should be = " + result
+                    + " sides " + sides[i, 0] + " " + sides[i, 1] + " " + sides[i, 2] + " p " + p);
+            }
+        }
+        [TestMethod]
+        public void TestTriangleBaseAndHeight()
+        {
+            double[,] sides = { { 0.1, 0.2 },
+                                { 0.3, 0.4 },
+                                { 0.5, 0.6 } };
+            for (int i = 0; i < sides.GetLength(0); i++)
+            {
+                double result = 0.5 * sides[i, 0] * sides[i, 1];
+                double fromLibrary = AreasLibrary.AreasShapes.AreaTriangle(sides[i, 0], sides[i, 1]);
+                Assert.AreEqual(fromLibrary, result, 0.01, "From library = " + fromLibrary + " should be = " + result);
+            }
+        }
+        [TestMethod]
+        public void TestIsRightTriangleTrue()
+        {
+            double[,] sides = { { 5, 4, 3 } };
+            for (int i = 0; i < sides.GetLength(0); i++)
+            {                
+                bool fromLibrary = AreasLibrary.AreasShapes.isRightTriangle(sides[i, 0], sides[i, 1], sides[i, 2]);
+                Assert.IsTrue(fromLibrary);
+            }
+        }
+        [TestMethod]
+        public void TestIsRightTriangleFalse()
+        {
+            double[,] sides = { { 8, 4, 3 } };
+            for (int i = 0; i < sides.GetLength(0); i++)
+            {
+                bool fromLibrary = AreasLibrary.AreasShapes.isRightTriangle(sides[i, 0], sides[i, 1], sides[i, 2]);
+                Assert.IsFalse(fromLibrary);
+            }
+        }
+    }
+}
