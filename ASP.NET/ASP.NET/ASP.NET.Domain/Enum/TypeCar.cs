221a12a57e736561317408e4618810ad72a1cff6
timhaX <timaan2000@gmail.com>
2022-06-24 12:54:36 +0300

    ASP.NET Project

---
+++ ASP.NET/ASP.NET/ASP.NET.Domain/Enum/TypeCar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASP.NET.Domain.Enum
+{
+    public enum TypeCar
+    {
+        [Display(Name ="Легковой автомобиль")]
+        PassengerCar=0,
+        [Display(Name = "Седан")]
+        Sedan = 1,
+        [Display(Name = "Хетчбэк")]
+        HatchBack = 2,
+        [Display(Name = "Минивэен")]
+        Minivan = 3,
+        [Display(Name = "Спортивная машина")]
+        SportsCar = 4,
+        [Display(Name = "Внедорожник")]
+        Suv = 5
+    }
+}
---
+++ ASP.NET/ASP.NET/ASP.NET/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using ASP.NET.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP.NET.Domain.Models;
+
+namespace ASP.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Car car = new Car()
+            {
+                Name = "Alex",
+                Speed = 320
+            };
+            return View(car);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
